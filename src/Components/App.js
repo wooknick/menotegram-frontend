bffa1b7f13de9ab5b568c4ed98022cc0a1e188f7
wooknick <wooknick@naver.com>
2019-07-02 17:06:27 +0900

    Add Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import GloabalStyles from "../Styles/GloabalStyles";
 import Theme from "../Styles/Theme";
+import Router from "./Router";
 
 export default () => (
     <ThemeProvider theme={Theme}>
-        <GloabalStyles />
+        <>
+            <GloabalStyles />
+            <Router isLoggedIn={true} />
+        </>
     </ThemeProvider>
 );
